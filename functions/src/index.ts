36c15dc6f97169d03cbf056a3d74f6d06a8c9394
axlfrost <ale.gonzalezlevio@gmail.com>
2019-12-13 15:15:15 -0300

    Se añade la obtención de juegos

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,10 @@ admin.initializeApp({
   databaseURL: "https://firestore-grafica-e82b8.firebaseio.com"
 });
 
+// Referencia a la base de datos
+const db = admin.firestore();
+
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -18,3 +22,16 @@ export const helloWorld = functions.https.onRequest((request, response) => {
      message: "¡Hola mundo desde Funciones de Firebase!"
  });
 });
+
+
+export const getGOTY = functions.https.onRequest( async (request, response) => {
+
+    // Referencia a la colección
+    const gotyRef = db.collection('goty');
+    // ¡Obtención de la información en el momento y preciso instante!
+    const documentSnap = await gotyRef.get();    
+    // Documentos procesados 
+    const juegos = documentSnap.docs.map(doc => doc.data());
+    response.json(juegos);
+
+   });
